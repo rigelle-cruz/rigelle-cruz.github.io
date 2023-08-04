ed7e9b561530e94bf5ff9456b93602cd69d6f29d
Rigelle Cruz <rigellecruz@gmail.com>
2023-08-04 14:55:34 +1200

    complete video popup

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
 $(document).ready(function() {
-  // Show the video player when the image is clicked
   $(".container img").click(function() {
     const videoUrl = $(this).data("video");
     $("#videoPlayer").attr("src", videoUrl);
     $("#videoPopup").fadeIn();
   });
 
-  // Hide the video player when the close button is clicked
   $("#videoPopup").click(function(event) {
     if ($(event.target).is("#videoPopup")) {
       hideVideoPlayer();
     }
   });
 
-  // Hide the video player when the escape key is pressed
   $(document).keydown(function(event) {
     if (event.key === "Escape") {
       hideVideoPlayer();
