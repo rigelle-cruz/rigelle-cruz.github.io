ad2bf2849659139427e29c50d870c4ef447beb0c
Rigelle Cruz <rigellecruz@gmail.com>
2023-08-04 14:01:12 +1200

    modify js file for video popup and modify css

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
   // Show the video player when the image is clicked
   $(".container img").click(function() {
-      const videoUrl = $(this).data("video");
-      $("#videoPlayer").attr("src", videoUrl);
-      $("#videoPopup").fadeIn();
+    const videoUrl = $(this).data("video");
+    $("#videoPlayer").attr("src", videoUrl);
+    $("#videoPopup").fadeIn();
   });
 
   // Hide the video player when the close button is clicked
   $("#videoPopup").click(function(event) {
-      if ($(event.target).closest("#videoPlayer").length === 0) {
-          hideVideoPlayer();
-      }
+    if ($(event.target).is("#videoPopup")) {
+      hideVideoPlayer();
+    }
   });
 
   // Hide the video player when the escape key is pressed
   $(document).keydown(function(event) {
-      if (event.key === "Escape") {
-          hideVideoPlayer();
-      }
+    if (event.key === "Escape") {
+      hideVideoPlayer();
+    }
   });
 });
 
